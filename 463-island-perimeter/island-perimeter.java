803136730657711cae4e0e0e1391cfed64cf52bd
haripriyaprakash3004 <156827067+haripriyaprakash3004@users.noreply.github.com>
2025-03-24 19:29:19 +0530

    Time: 4 ms (99.86%) | Memory: 45.7 MB (67.70%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,26 +1,20 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
         int perimeter = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) {
-                    perimeter += 4; // Assume all four sides contribute to perimeter
-
-                    // Check top neighbor
-                    if (i > 0 && grid[i - 1][j] == 1) {
-                        perimeter -= 2; // Subtract 2 for shared edge
-                    }
-
-                    // Check left neighbor
-                    if (j > 0 && grid[i][j - 1] == 1) {
-                        perimeter -= 2; // Subtract 2 for shared edge
-                    }
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    perimeter += 4;
+                
+                if(i>0 && grid[i-1][j] == 1){
+                    perimeter -= 2;
+                }
+                if(j>0 && grid[i][j-1] == 1){
+                    perimeter -= 2;
                 }
             }
-        }
+            }
+        }       
         return perimeter;
     }
 }
