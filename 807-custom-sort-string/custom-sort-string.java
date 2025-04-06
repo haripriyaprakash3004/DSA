0c7ccf010f00a55fa1f8249372d58cd66726fc48
haripriyaprakash3004 <156827067+haripriyaprakash3004@users.noreply.github.com>
2025-04-06 22:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (25.78%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int freq[] = new int[26];
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            freq[c - 'a']++;
+        }
+        for(char c : order.toCharArray()){
+            while(freq[c-'a'] > 0){
+                sb.append(c);
+                freq[c - 'a']--;
+            }
+        }
+        for(char c : s.toCharArray()){
+            if(freq[c-'a'] != 0){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
