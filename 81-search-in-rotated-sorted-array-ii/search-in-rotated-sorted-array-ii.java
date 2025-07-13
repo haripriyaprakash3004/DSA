1413faf11584fd2b28d8b926e79744d5e110cbea
haripriyaprakash3004 <156827067+haripriyaprakash3004@users.noreply.github.com>
2025-07-13 19:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (50.29%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -6,19 +6,47 @@ class Solution {
         //     }
         // }
         // return false;
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // int low = 0;
+        // int high = nums.length-1;
+        // while(low <= high){
+        //     int mid = (low+high)/2;
+        //     if(nums[mid] == target){
+        //         return true;
+        //     }
+        //     else if(nums[mid] < target){
+        //         low = mid+1;
+        //     }
+        //     else {
+        //         high = mid-1;
+        //     }
+        // }
+        // return false;
         int low = 0;
         int high = nums.length-1;
         while(low <= high){
             int mid = (low+high)/2;
-            if(nums[mid] == target){
-                return true;
+            if(nums[mid] == target) return true;
+            if(nums[mid] == nums[low] && nums[mid]==nums[high]){
+                low = low+1;
+                high = high-1;
+                continue;
             }
-            else if(nums[mid] < target){
-                low = mid+1;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
-            else {
-                high = mid-1;
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return false;
