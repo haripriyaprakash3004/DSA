e61ee2965fd3be9a43f0a9376f85944bf14cb804
Haripriya P <156827067+haripriyaprakash3004@users.noreply.github.com>
2025-10-07 12:24:30 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (40.59%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int h = 0;
+        for(int i=citations.length-1;i>=0;i--){
+            if(citations[i] > h) h++;
+            else{
+                return h;
+            }
+        }
+        return h;
+    }
+}
